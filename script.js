faa5808dff16adc4cfc9f6bee713b8a9e6ff2786
Ryan Mohta <ryanmohta@gmail.com>
2019-03-02 12:57:30 -0800

    Updated to match Safari extension

---
+++ script.js
@@ -0,0 +1,17 @@
+var body = document.getElementsByTagName("BODY")[0];
+
+document.addEventListener("DOMContentLoaded", myTimer);
+
+var timerVariable = setInterval(myTimer, 1000);
+
+function myTimer() {
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    if(hour < 7 || /*(hour > 16 && minute > 29) || */hour > 17) {
+        document.getElementsByTagName("BODY")[0].classList.add("blackMode");
+    }
+    else {
+        document.getElementsByTagName("BODY")[0].classList.remove("blackMode");
+    }
+}
