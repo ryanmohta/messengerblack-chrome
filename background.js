97d203bfcba67f55219d3b96d2b1f17be7b0baef
Ryan Mohta <ryanmohta@gmail.com>
2020-12-26 20:49:57 -0800

    Moved dynamic stylesheet insertion to activate_icon

--- background.js
+++ background.js
@@ -2,50 +2,44 @@ var timerVariable;
 
 // Initialization
 chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install") {
-      chrome.storage.sync.set({"manual": true});
-      chrome.storage.sync.set({"onOff": true});
+  if(details.reason == "install") {
+    chrome.storage.sync.set({"manual": true});
+    chrome.storage.sync.set({"onOff": true});
 
-      chrome.storage.sync.set({"scheduled": false});
-      chrome.storage.sync.set({"startTime": "19:30"});
-      chrome.storage.sync.set({"endTime": "07:00"});
+    chrome.storage.sync.set({"scheduled": false});
+    chrome.storage.sync.set({"startTime": "19:30"});
+    chrome.storage.sync.set({"endTime": "07:00"});
 
-      chrome.storage.sync.set({"sunsetToSunrise": false});
-    }
+    chrome.storage.sync.set({"sunsetToSunrise": false});
+  }
 });
-
-// Check if extension is run on facebook.com
-chrome.webNavigation.onDOMContentLoaded.addListener(function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.storage.sync.get(['state'], function(result) {
-      chrome.tabs.insertCSS(tabs[0].id, {
-        file: "node_modules/@messengerblack/messengerblack-css/base.css"
-      });
-    });
-  });
-}, {url: [{hostContains: '.facebook'}]});
-
-// Check if extension is run on messenger.com
-chrome.webNavigation.onDOMContentLoaded.addListener(function() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.storage.sync.get(['state'], function(result) {
-      chrome.tabs.insertCSS(tabs[0].id, {
-        file: "node_modules/@messengerblack/messengerblack-css/base.css"
-      });
-      chrome.tabs.insertCSS(tabs[0].id, {
-        file: "node_modules/@messengerblack/messengerblack-css/svg.css"
-      });
-    });
-  });
-}, {url: [{hostContains: '.messenger'}]});
-
 
 chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
+  function(request, sender, sendResponse) {
     if (request.message === "activate_icon") {
-        chrome.pageAction.show(sender.tab.id);
+      chrome.pageAction.show(sender.tab.id);
+
+      chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+
+        // Check if extension is run on facebook.com
+        if (url.includes('facebook.com')) {
+          chrome.tabs.insertCSS(tabs[0].id, {
+            file: "node_modules/@messengerblack/messengerblack-css/base.css"
+          });
+        } // Check if extension is run on messenger.com
+        else if (url.includes('messenger.com')) {
+          chrome.tabs.insertCSS(tabs[0].id, {
+            file: "node_modules/@messengerblack/messengerblack-css/base.css"
+          });
+          chrome.tabs.insertCSS(tabs[0].id, {
+            file: "node_modules/@messengerblack/messengerblack-css/svg.css"
+          });
+        }
+      });
     }
-});
+  }
+);
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -63,11 +57,11 @@ chrome.runtime.onMessage.addListener(
     return true;
   });
 
-function getLocation() {
-  navigator.geolocation.getCurrentPosition(function(position) {
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(function(position) {
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {name: "sunsetToSunrise", latitude: position.coords.latitude, longitude: position.coords.longitude});
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {name: "sunsetToSunrise", latitude: position.coords.latitude, longitude: position.coords.longitude});
+      });
     });
-  });
-}
+  }
